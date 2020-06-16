1f952a95010fb21a348a6ed620b97b691a2ac7a7
Gilson Filho <gilsonpcf@gmail.com>
2020-06-16 14:31:41 -0300

    Update ArvoreBinariaBusca.java

--- arvore/java/ArvoreBinariaBusca.java
+++ arvore/java/ArvoreBinariaBusca.java
@@ -66,6 +66,54 @@ public class ArvoreBinariaBusca {
 		return false;
 	}
 
+	public void remover(int valor) {
+		if (raiz != null) {
+			remover(valor, raiz, null);
+		}
+	}
+
+	public void remover(int valor, No raiz, No pai) {
+		if (valor == raiz.valor) {
+			if (raiz.esquerda == null && raiz.direita == null) {      // Nó não tem filhos
+				if (raiz == pai.esquerda)    // Nó é filho esquerdo?
+					pai.esquerda = null;
+				else
+					pai.direita = null;
+			}
+			else if (raiz.esquerda != null && raiz.direita == null) { // Nó tem um filho (esquerdo)
+				if (raiz == pai.esquerda)    // Nó é filho esquerdo?
+					pai.esquerda = raiz.esquerda;
+				else
+					pai.direita = raiz.esquerda;
+			}
+			else if (raiz.esquerda == null && raiz.direita != null) { // Nó tem um filho (direito)
+				if (raiz == pai.esquerda)    // Nó é filho esquerdo?
+					pai.esquerda = raiz.direita;
+				else
+					pai.direita = raiz.direita;
+			}
+			else if (raiz.esquerda != null && raiz.direita != null) { // Nó tem dois filhos
+				raiz.valor = menorValor(raiz.direita);
+				remover(raiz.valor, raiz.direita, raiz);
+			}
+		}
+		else if (valor < raiz.valor) {
+			if (raiz.esquerda != null)
+				remover(valor, raiz.esquerda, raiz);
+		}
+		else if (valor > raiz.valor) {
+			if (raiz.direita != null)
+				remover(valor, raiz.direita, raiz);
+		}
+	}
+
+	public int menorValor(No raiz) {
+		if (raiz.esquerda == null)
+			return raiz.valor;
+		else
+			return menorValor(raiz.esquerda);
+	}
+
 	public void exibirPreOrdem() {
 		if (raiz != null) {
 			exibirPreOrdem(raiz);
