313175aec6a04c2ab10a98beaf8937b70ce3c7a3
Gilson Filho <gilsonpcf@gmail.com>
2020-09-11 09:39:34 -0300

    Update ListaEstatica.java

--- lista/java/ListaEstatica.java
+++ lista/java/ListaEstatica.java
@@ -1,84 +1,83 @@
 public class ListaEstatica {
-	private int[] v;
-	private int tamanho;
+    private int[] v;
+    private int tamanho;
 
-	public ListaEstatica(int n) {
-		v = new int[n];
-		tamanho = 0;
-	}
+    public ListaEstatica(int n) {
+        v = new int[n];
+        tamanho = 0;
+    }
 
-	public void adicionarFinal(int valor) {
-		if (tamanho < v.length) {
-			v[tamanho] = valor;
-			tamanho++;
-		} else
-			System.out.println("Lista cheia!");
-	}
+    public void adicionarFinal(int valor) {
+        if (tamanho < v.length) {
+            v[tamanho] = valor;
+            tamanho++;
+        } else
+            System.out.println("Lista cheia!");
+    }
 
-	public void adicionarPosicao(int valor, int pos) {
-		if (pos < v.length) {
-			for (int i = tamanho; i > pos; i--)
-				v[i] = v[i-1];
+    public void adicionarPosicao(int valor, int pos) {
+        if (pos < v.length) {
+            for (int i = tamanho; i > pos; i--)
+                v[i] = v[i-1];
 
-			v[pos] = valor;
-			tamanho++;
-		} else
-			System.out.println("Posicao informada nao existe.");
-	}
+            v[pos] = valor;
 
-	public void adicionarInicio(int valor) {
-		adicionarPosicao(valor, 0);
-	}
+            tamanho++;
+        } else
+            System.out.println("Posicao informada nao existe.");
+    }
 
-	public void removerFinal() {
-		if (tamanho == 0)
-			System.out.println("Lista vazia!");
-		else
-			tamanho--;
-	}
+    public void adicionarInicio(int valor) {
+        adicionarPosicao(valor, 0);
+    }
 
-	public void removerPosicao(int pos) {
-		if (tamanho == 0)
-			System.out.println("Lista vazia!");
-		else if (pos < 0 || pos >= tamanho)
-			System.out.println("Posicao invalida!");
-		else {
-			for (int i = pos; i < tamanho-1; i++) {
-				v[i] = v[i+1];
-			}
-			tamanho--;
-		}
-	}
+    public void removerFinal() {
+        if (tamanho == 0)
+            System.out.println("Lista vazia!");
+        else
+            tamanho--;
+    }
 
-	public void removerInicio() {
-		removerPosicao(0);
-	}
+    public void removerPosicao(int pos) {
+        if (tamanho == 0)
+            System.out.println("Lista vazia!");
+        else if (pos < 0 || pos >= tamanho)
+            System.out.println("Posicao invalida!");
+        else {
+            for (int i = pos; i < tamanho-1; i++)
+                v[i] = v[i+1];
 
-	public int buscar(int valor) {
-		for (int i = 0; i < tamanho; i++) {
-			if (valor == v[i])
-				return i;
-		}
+            tamanho--;
+        }
+    }
 
-		return -1;
-	}
+    public void removerInicio() {
+        removerPosicao(0);
+    }
 
-	public void exibir() {
-		for (int i = 0; i < tamanho; i++) {
-			System.out.print(v[i] + " ");
-		}
+    public int buscar(int valor) {
+        for (int i = 0; i < tamanho; i++)
+            if (valor == v[i])
+                return i;
 
-		System.out.println();
-	}
+        return -1;
+    }
 
-	public boolean vazia() {
-		if (tamanho == 0)
-			return true;
-		else
-			return false;
-	}
+    public void exibir() {
+        for (int i = 0; i < tamanho; i++)
+            System.out.print(v[i] + " ");
 
-	public int getTamanho() {
-		return tamanho;
-	}
+        System.out.println();
+    }
+
+    public boolean vazia() {
+        if (tamanho == 0)
+            return true;
+        else
+            return false;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
 }
