e2195e8c831eda8399828bb3377bc8d16b283f9c
Gilson Filho <gilsonpcf@gmail.com>
2020-09-17 14:59:29 -0300

    Update AppLista.java

--- lista/java/AppLista.java
+++ lista/java/AppLista.java
@@ -3,17 +3,21 @@ public class AppLista {
         //ListaEstatica lista = new ListaEstatica(10);
         ListaDinamica lista = new ListaDinamica();
 
-        lista.adicionarInicio(5);
+        lista.adicionarFinal(30);
         lista.exibir();
-        lista.adicionarInicio(1);
+        lista.adicionarFinal(13);
         lista.exibir();
-        lista.adicionarFinal(16);
+        lista.adicionarFinal(1);
         lista.exibir();
-        lista.adicionarFinal(2);
+        lista.adicionarFinal(9);
         lista.exibir();
-        lista.removerInicio();
+        lista.adicionarFinal(25);
+        lista.exibir();
+        lista.adicionarInicio(7);
         lista.exibir();
         lista.removerFinal();
+        lista.exibir();
+        lista.removerInicio();
         lista.exibir();
     }
 }
