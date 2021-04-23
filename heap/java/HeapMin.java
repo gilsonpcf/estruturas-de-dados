cdc4b703e989c6ded94355ec6a23bc1f84fee697
Gilson Pereira <gilsonpcf@gmail.com>
2021-04-23 01:04:19 -0300

    Update HeapMin.java

--- heap/java/HeapMin.java
+++ heap/java/HeapMin.java
@@ -1,90 +1,87 @@
 public class HeapMin {
-	private int[] dados;
-	private int tamanho;
+    private int[] dados;
+    private int contador;
 
-	public HeapMin(int n) {
-		dados = new int[n];
-		tamanho = 0;
-	}
+    public HeapMin(int n) {
+        dados = new int[n];
+        contador = 0;
+    }
 
-	public int menor() {
-		if (vazio())
-			throw new RuntimeException("Heap vazio");
-		else
-			return dados[0];
-	}
+    public int menor() {
+        if (vazio())
+            throw new RuntimeException("Heap vazio");
 
-	public boolean vazio() {
-		return (tamanho == 0);
-	}
+        return dados[0];
+    }
 
-	public void inserir(int valor) {
-		if (tamanho == dados.length)
-			throw new RuntimeException("Heap lotado");
-		else {
-			tamanho++;
-			dados[tamanho - 1] = valor;
-			subir(tamanho - 1);
-		}
-	}    
+    public boolean vazio() {
+        return (contador == 0);
+    }
 
-	private void subir(int indice) {
-		int indicePai, temp;
+    public void inserir(int valor) {
+        if (contador == dados.length)
+            throw new RuntimeException("Heap lotado");
 
-		if (indice != 0) {
-			indicePai = (indice - 1) / 2;
+        dados[contador] = valor;
+        contador++;
+        subir(contador - 1);
+    }
 
-			if (dados[indicePai] > dados[indice]) {
-				temp = dados[indicePai];
-				dados[indicePai] = dados[indice];
-				dados[indice] = temp;
-				subir(indicePai);
-			}
-		}
-	}
+    private void subir(int indice) {
+        int indicePai, temp;
 
-	public void remover() {
-		if (vazio())
-			throw new RuntimeException("Heap vazio");
-		else {
-			dados[0] = dados[tamanho - 1];
-			tamanho--;
-			if (tamanho > 0)
-				descer(0);
-		}
-	}
+        if (indice != 0) {
+            indicePai = (indice - 1) / 2;
 
-	private void descer(int indice) {
-		int indiceFilhoEsq, indiceFilhoDir, indiceMin, temp;
+            if (dados[indicePai] > dados[indice]) {
+                temp = dados[indicePai];
+                dados[indicePai] = dados[indice];
+                dados[indice] = temp;
+                subir(indicePai);
+            }
+        }
+    }
 
-		indiceFilhoEsq = 2 * indice + 1;
-		indiceFilhoDir = 2 * indice + 2;
+    public void remover() {
+        if (vazio())
+            throw new RuntimeException("Heap vazio");
 
-		if (indiceFilhoDir >= tamanho) {
-			if (indiceFilhoEsq >= tamanho)
-				return;
-			else
-				indiceMin = indiceFilhoEsq;
-		} else {
-			if (dados[indiceFilhoEsq] <= dados[indiceFilhoDir])
-				indiceMin = indiceFilhoEsq;
-			else
-				indiceMin = indiceFilhoDir;
-		}
+        dados[0] = dados[contador - 1];
+        contador--;
+        if (contador > 0)
+            descer(0);
+    }
 
-		if (dados[indice] > dados[indiceMin]) {
-			temp = dados[indiceMin];
-			dados[indiceMin] = dados[indice];
-			dados[indice] = temp;
-			descer(indiceMin);
-		}
-	}
+    private void descer(int indice) {
+        int indiceFilhoEsq, indiceFilhoDir, indiceMin, temp;
 
-	public void exibir() {
-		for (int i = 0; i < tamanho; i++) {
-			System.out.print(dados[i] + " ");
-		}
+        indiceFilhoEsq = 2 * indice + 1;
+        indiceFilhoDir = 2 * indice + 2;
 
-		System.out.println();
-	}
+        if (indiceFilhoDir >= contador) {
+            if (indiceFilhoEsq >= contador)
+                return;
+            else
+                indiceMin = indiceFilhoEsq;
+        } else {
+            if (dados[indiceFilhoEsq] <= dados[indiceFilhoDir])
+                indiceMin = indiceFilhoEsq;
+            else
+                indiceMin = indiceFilhoDir;
+        }
+
+        if (dados[indice] > dados[indiceMin]) {
+            temp = dados[indiceMin];
+            dados[indiceMin] = dados[indice];
+            dados[indice] = temp;
+            descer(indiceMin);
+        }
+    }
+
+    public void exibir() {
+        for (int i = 0; i < contador; i++)
+            System.out.print(dados[i] + " ");
+
+        System.out.println();
+    }
 }
